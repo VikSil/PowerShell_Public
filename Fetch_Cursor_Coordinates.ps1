69ea8a767f070a6399926796c3b0c32225bae992
VikSil <viktoria.sudraba@gmail.com>
2023-06-20 21:22:30 +0100

    Initialising powerShell_Public remote repo and adding Mouse Coordinates script

---
+++ Fetch_Cursor_Coordinates.ps1
@@ -0,0 +1,18 @@
+﻿<#
+This script will help you capture cursor coordinates on the screen
+Run the script and move the cursor the the spot that you want to know the coordinates for
+Wait for three seconds for the coordinates to appear in the console
+#>
+
+Add-Type -AssemblyName System.Windows.Forms
+
+sleep 3
+
+$X = [System.Windows.Forms.Cursor]::Position.X
+$Y = [System.Windows.Forms.Cursor]::Position.Y
+ 
+Write-Output "X: $X | Y: $Y"
+
+
+
+
