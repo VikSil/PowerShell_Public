fc134716f197add47942d79cca1ce1a662e0a95a
Viktoria Silver <viktoria.sudraba@gmail.com>
2023-07-01 14:42:49 +0100

    Added Kill Process Script

---
+++ Kill_Process_By_Name_and_CommandLine.ps1
@@ -0,0 +1,7 @@
+﻿$procName = 'notepad.exe'
+$cmdLine  = "*notepad*"
+
+
+$process = gcim win32_process| Where-Object {$_.ProcessName -eq $procName -and $_.CommandLine -like $cmdLine } 
+
+taskkill /pid $process.processid
